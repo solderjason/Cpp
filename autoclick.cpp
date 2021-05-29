1ac3f69fa310b6b7c35b1b47e8183dbfe28ef495
solderjason <solderjason@hotmail.com>
2021-05-28 18:00:29 -0700

    cleaned up code

--- autoclick.cpp
+++ autoclick.cpp
@@ -1,16 +1,17 @@
 #include <windows.h>
 #include <iostream>
 #include <random>
+using namespace std;
 bool clicker;
 int rand;
 bool random;
 
-void location(int x, int y){
+//void location(int x, int y){
 
-}
+//}
 void options()
 {
-
+std::cout<< "Press '/' to enable left click, 'r' to enable randomized left_click and '\' to turn off autoclick ";
 }
 
 void left_click(){
@@ -20,26 +21,37 @@ void left_click(){
       clicker=true;
     }
 
-    if(GetAsyncKeyState('\') ){
+  else  if(GetAsyncKeyState('\') ){
       clicker=false;
     }
+    else  if(GetAsyncKeyState('r') ){
+        clicker=true;
+        random=true;
+      }
 if(clicker==true){
   mouse_event( MOUSEEVENTF_LEFTDOWN, 0, 0, 0 );
 
   Sleep( 50 );
   mouse_event( MOUSEEVENTF_LEFTUP, 0, 0, 0 );
 }
-if (clicker==true && random==true)
-}
-}
+if (clicker==true && random==true){
+  mouse_event( MOUSEEVENTF_LEFTDOWN, 0, 0, 0 );
 
+  Sleep( rand() %50 );
+  mouse_event( MOUSEEVENTF_LEFTUP, 0, 0, 0 );
+}
+}
+}
+/*
 void right_click(){
   mouse_event( MOUSEEVENTF_RIGHTDOWN, 0, 0, 0 );
 
   Sleep( 50 );
   mouse_event( MOUSEEVENTF_RIGHTP, 0, 0, 0 );
 }
-
+*/
 int main(){
-
+options();
+left_click();
+return 0;
 }
--- dice.cpp
+++ dice.cpp
@@ -1,14 +1,39 @@
 //Dice Roller
+
 #include <iostream>
-#include <vector>
-#include <iostream>
+#include <ctime>
+#include <random>
+#include <string>
+#include <chrono>
 using namespace std;
 
-int roll(int faces)
-{
 
+int main(){
+  srand(time(NULL));
+
+int faces=0;
+int rolls=0;
+int result=0;
+
+std::cout<<"Enter the number of faces:"
+std::cin>> faces
+std::cout<<"Enter the number of rolls:"
+
+std::cin>> rolls
+
+
+
+for (int i = 0; i <rolls; i++) {
+  result=rand() %number +1 //+1 so its from 1-n
+  std::cout<<"You rolled "+'result'<<std::endl
+
+  auto timenow =
+     chrono::system_clock::to_time_t(chrono::system_clock::now());
+
+   cout << ctime(&timenow) << endl;
+  return 0;
 }
-int main()
-{
 
+  system("PAUSE")'
+  return 0;
 }
